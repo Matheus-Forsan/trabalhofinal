2f39b2a9d32cf60c8e352a6c1054234a01fccc9f
Matheus-Forsan <matheusforsan@gmail.com.com>
2024-10-18 15:28:58 -0300

    detalhes

--- src/pages/DetalhesProduto.js
+++ src/pages/DetalhesProduto.js
@@ -1,9 +1,54 @@
 import React from "react";
+import Cabecalho from "../components/Cabeçalho";
+import leg from "../assets/images/legpreta.jpeg.png"
+
+import "../styles/detalhes.css"
+
+
 
 export default function DetalhesProduto() {
   return (
     <div>
-      <h1>DetalhesProduto</h1>
+      <Cabecalho />
+      
+        <div className="produtogrid">
+        <div className="imagemproduto">
+            <img src={leg} />
+        </div>
+        <div className="produtoinfo">
+          <h1>Calça legging - preta simples</h1>
+          <div className="av">
+          <h3>Avaliações: </h3><p>4,6</p>
+          </div>
+          <div className="price">
+          <h2>R$59,90</h2>
+          </div>
+          <div className="est">
+            <h3>Em estoque:12</h3>
+          </div>
+          <div className="size">
+            <h2>Tamanho:</h2>
+            <select>
+              <option>P</option>
+              <option>M</option>
+              <option>G</option>
+            </select>
+          </div>
+          <div className="size">
+            <h2>Quantidade:</h2>
+            <select>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+            </select>
+          </div>
+          <div className="bd"> 
+          <button className="addtocart" onClick={() => alert("onclick oooo")}>
+            <h2>Adicionar ao carrinho</h2>
+          </button>
+          </div>
+        </div>
+        </div>
     </div>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -17,6 +17,7 @@ export default function AppRoutes() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/detalhes" element={<DetalhesProduto />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
         <Route path="/produto/:id" element={<DetalhesProduto />} />
